2350a04535a4adc38fa7cdd81f162408957a3ae3
kierangoodacre <k.goodacre@hotmail.co.uk>
2014-12-25 15:44:58 +0000

    added and passed test for checking menu upon receiving order

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -4,4 +4,12 @@ class Restaurant
     @menu = {"burger" => 5.00, "chips" => 2.00, "coke" => 1.00}
   end
 
+  def receive_order item
+    @receive_order = :ordered
+  end
+
+  def check_menu
+    @receive_order
+  end
+
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -8,4 +8,14 @@ describe Restaurant do
     expect(restaurant.menu).to include("burger" => 5.00)
   end
 
+  it "should be able to receive order" do
+    expect(restaurant.receive_order("burger")).to eq(:ordered)
+  end
+
+  it "should be able to check the menu when reeiving order" do
+    restaurant.menu
+    restaurant.receive_order("burger")
+    expect(restaurant.check_menu).to eq :ordered
+  end
+
 end
