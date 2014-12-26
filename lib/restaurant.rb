1f14749a357c5b1278c2772facca9337c0febb46
kierangoodacre <k.goodacre@hotmail.co.uk>
2014-12-26 07:57:44 +0000

    added check menu functionality and raise error if not on the menu

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,15 +1,21 @@
 class Restaurant
 
+  def initialize
+    menu
+  end
+
   def menu
-    @menu = {"burger" => 5.00, "chips" => 2.00, "coke" => 1.00}
+    @menu = {burger: 5.00, chips: 2.00, coke: 1.00}
   end
 
   def receive_order item
-    @receive_order = :ordered
-  end
-
-  def check_menu
-    @receive_order
+    raise "Sorry not in stock" if @menu.key?(item) == false
+    :ordered if @menu.key?(item) 
+    # if @menu.key?(item)
+    #   :ordered
+    # else
+    #  :not_on_the_menu
+    # end
   end
 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -5,17 +5,15 @@ describe Restaurant do
   let (:restaurant) {Restaurant.new}
 
   it "should have a menu on initialization" do
-    expect(restaurant.menu).to include("burger" => 5.00)
+    expect(restaurant.menu).to include(burger: 5.00)
   end
 
-  it "should be able to receive order" do
-    expect(restaurant.receive_order("burger")).to eq(:ordered)
+  it "should be able to receive correct order" do
+    expect(restaurant.receive_order(:burger)).to eq(:ordered)
   end
 
-  it "should be able to check the menu when reeiving order" do
-    restaurant.menu
-    restaurant.receive_order("burger")
-    expect(restaurant.check_menu).to eq :ordered
+  it "should raise error if not on the menu" do
+    expect( lambda {restaurant.receive_order(:chicken)}).to raise_error(RuntimeError, "Sorry not in stock")
   end
 
 end
