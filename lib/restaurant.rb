0cbb0dae0f66bfb9b1d925352aba53db08f9756c
kierangoodacre <k.goodacre@hotmail.co.uk>
2014-12-30 10:17:31 +0000

    user can now place order for multiple items

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -14,12 +14,11 @@ class Restaurant
   end
 
   def menu
-    @menu = {burger: 5.00, chips: 2.00, coke: 1.00}
+    @menu = {"burger" => 5.00, "chips" => 2.00, "coke" => 1.00}
   end
 
-  def receive_order *items
-    raise "Sorry not in stock" if @menu.key?(*items) == false
-    @bill << items
+  def receive_order(*items)
+    raise "Sorry not in stock" if items.all? { |key|@menu.has_key?(key) } == false
     :ordered if @menu.fetch(*items) 
   end
 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,13 +7,13 @@ describe User do
   let (:restaurant) {Restaurant.new}
   let(:user) {User.new(restaurant)}
 
-  it "should be able to order a burger" do
-    expect(user.place_order(:burger)).to eq(:ordered)
+  it "should be able to order multiple items from menu" do
+    expect(user.place_order("burger", "chips")).to eq(:ordered)
   end
 
   it "should be able to request a menu" do
     user.menu_request
-    expect(user.menu_request).to include :burger
+    expect(user.menu_request).to include "burger"
   end
 
 end
