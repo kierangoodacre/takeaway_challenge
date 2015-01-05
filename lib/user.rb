e1b10ebe149774cee9081cf9e95b6d2c313ddda8
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-05 17:54:53 +0000

    intergrated msg class with user

--- lib/user.rb
+++ lib/user.rb
@@ -1,16 +1,11 @@
-require 'twilio-ruby'
-
 require_relative 'restaurant'
 require_relative 'menu'
+require_relative 'message'
 
 class User
 
-  account_sid = 'ACd622d7c9b5bb8dbb0d12d9e40d9ae827'
-  auth_token = '8c9369a8312bc9c6bca0a711cf618f5a'
+  attr_reader :restaurant, :menu, :message
 
-  attr_reader :restaurant, :menu
-
-  @client = Twilio::REST::Client.new account_sid, auth_token
 
   def initialize restaurant, menu
     @restaurant = restaurant
